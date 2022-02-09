7c4e5b841cf614bc9f67b3fde01e1034d4facdb9
Christopher Backes <Christopher.Backes@gmail.com>
2022-02-08 19:09:16 -0500

    got recursion working, problem with console display

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 const cTable = require("console.table");
+const inquirer = require("inquirer");
 const mysql = require("mysql2");
 const db = require("./db/connection");
-const prompt = require('./utils/prompt')
 const redirectQuestion = require("./utils/redirectQuestion");
 
+const list = [
+	"View All Departments",
+	"View All Roles",
+	"View All Employees",
+	"Add a Department",
+	"Add a Role",
+	"Add an Employee",
+	"Update Employee Role",
+    "Exit"
+];
 
+function init() {
+	console.log(`
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~
+    What would you like to do?
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~
+    `);
+	inquirer.prompt({
+		type: "list",
+		name: "options",
+		message: "Select one",
+		choices: list,
+	})
+    .then((data) => {
+        redirectQuestion(data.options)
+    })
+    .then(() => {
+        init()
+    })
+    .catch(err => {
+        console.log(err);
+    })
+};
 
-prompt()
-.then((data) => {
-    return redirectQuestion(data.options);
-})
-.catch(err => {
-    console.log(err);
-});
+init();
